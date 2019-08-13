ec638c8cc1211513952d510a3636d5633c4da091
dalena <dalenaxtran@gmail.com>
2019-08-12 23:23:55 -0700

    Added Catalog, Events/Google Calendar API, Contact Form

--- themes/dimension/assets/js/marscalendar.js
+++ themes/dimension/assets/js/marscalendar.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
             // center: 'title',
             right: 'month,listYear,prev,next today'
         },
-        googleCalendarApiKey: 'AIzaSyBj1TiofAvw2KmBTzzJMRT5R5ZcVnAeqyw',
+        googleCalendarApiKey: 'AIzaSyCwlvxdMD9lphJtWLy2Lm71iWsZZYMsmnc',
         events: {
             googleCalendarId: 'g.ucla.edu_4l34ogt0tbh7isftkqb1t7itns@group.calendar.google.com',
             className: 'gcal-event', // an option!
